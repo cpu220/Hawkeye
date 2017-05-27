5289bff64e2ec32779434abbe5f7f6a60cb9e092
belial <102864814@qq.com>
2017-05-27 15:35:42 +0800

    add index

--- gulpfile.js
+++ gulpfile.js
@@ -4,30 +4,34 @@ const common = require('cpwcom');
 const connect = require('gulp-connect');
 const cfiles = require('cfiles');
 const project = require('./project.json');
+const less = require('gulp-less')
 
 const getFile = require('getFiles');
 
 const cf = new cfiles();
+
 const getfile = new getFile(project[0].url, ['jsx','js']);
 
 let report= [];
+let timer = null;
+
 const time=common.tools.formatDate(new Date()).split(' ')[0];
 
+gulp.task('default',['eslint','s'],function(){});
+
+// 启动服务
 gulp.task('s',  (req, res) =>{
   connect.server();
 });
 
 
-// let flg=true;// 进程初始化 true=>完毕 false=> 进程中
 
-let timer = null;
 
-gulp.task('default',['eslint','s'],function(){});
+
+
 gulp.task('eslint',function(){
 
   var promiseAll = project.map(item=>{
-
-
 
     report.push({
       name:item.name,
@@ -40,51 +44,48 @@ gulp.task('eslint',function(){
       root:item.url,
       suffix:['js','jsx'],
       callback: function (list) {
-
-
-        Promise.all(list.map(file=>{
-
-          new Promise((resolve,reject)=>{
-            gulp.src(file).pipe(eslint()).pipe(eslint.format('json',result=>{
-              const pname = file.substring(file.indexOf('store')).split('/')[1]
-              const obj = JSON.parse(result)[0];
-              obj.pname=pname;
-              clearTimeout(timer);
-              let dir = `result/${pname}/${time}/${file.split('/').join('_')}.json`;
-
-
-              cf.create(dir,JSON.stringify(obj));
-              resolve(obj);
-            }));
-          }).then((json)=>{
-
-
-
-            setStore(json);
-
-            timer = setTimeout(function(){
-              clearTimeout(timer);
-              console.log('=========end==============');
-              console.log(report);
-              cf.create(`result/all.json`,JSON.stringify(report));// todo 还需要扩展
-            },2000);
-
-          });
-
-
-        }))
-
+        eslintList(list,report);
       }
 
     });
-
-
 
   });
 
   Promise.all(promiseAll).then();
 
 });
+
+function eslintList(list,arr){
+
+  Promise.all(list.map(file=>{
+
+    var filePromise = new Promise((resolve,reject)=>{
+      gulp.src(file).pipe(eslint()).pipe(eslint.format('json',result=>{
+        const pname = file.substring(file.indexOf('store')).split('/')[1];// 项目名称
+        const obj = JSON.parse(result)[0];
+        obj.pname=pname;// 为了做异步区分，所以对obj进行了重赋值
+        clearTimeout(timer);
+        let dir = `result/${pname}/${time}/${file.split('/').join('_')}.json`;
+
+
+        cf.create(dir,JSON.stringify(obj));
+        resolve(obj);
+      }));
+    });
+
+    filePromise.then((json)=>{
+      // 哨兵变量
+      setStore(json);
+      timer = setTimeout(function(){
+        clearTimeout(timer);
+        console.log('=========end==============');
+        cf.create('result/all.json',JSON.stringify(arr));// todo 还需要扩展
+      },2000);
+    });
+
+
+  }));
+}
 
 function setStore(json){
   for(var i=0;i< report.length;i++){
@@ -104,5 +105,11 @@ function setStore(json){
   }
 
 }
+
+/*以下为工程测试代码*/
+gulp.task('less',function(){
+  gulp.src(['src/index/*.less']).pipe(less()).pipe(gulp.dest('src/dist'));
+
+});
 
 
---
+++ src/dist/index.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  animate.init();;
+});
+
+
+
+const animate={
+  arr:['A','B'],
+  init:function(){
+    var name= this.arr[parseInt(Math.random()*2)];
+    this[name]();
+  },
+  A:function(){
+    var config = {
+      vx: 4,//点x轴速度,正为右，负为左
+      vy:  4,//点y轴速度
+      height: 2,//点高宽，其实为正方形，所以不宜太大
+      width: 2,
+      count: 100,//点个数
+      color: "121, 162, 185",//点颜色
+      stroke: "130,255,255",//线条颜色
+      dist: 6000,//点吸附距离
+      e_dist: 2000,//鼠标吸附加速距离
+      max_conn: 10//点到点最大连接数
+    }
+    //调用
+    CanvasParticle(config);
+  },
+  B:function(){
+    $('#canvasContent').fireworks({
+      sound: true, // 声音效果
+      opacity: 0.9,
+      width: '100%',
+      height: '31.25rem',
+      zIndex:'-1'
+    });
+  }
+}
