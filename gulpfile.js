b527b4fb2965479974ed2acd84e5eca49e6f9d6d
belial <102864814@qq.com>
2017-06-11 22:45:11 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,62 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const shell = require('gulp-shell');
-
+const fs = require('fs');
 const common = require('cpwcom');
 const connect = require('gulp-connect');
 const cfiles = require('cfiles');
 
-const project = require('./project.json');// 工程目录
-const white = require('./whiteList.json');// 白名单
-const app = require('./package.json');// 包信息
+const project = require('./project.json'); // 工程目录
+const white = require('./whiteList.json'); // 白名单
+const app = require('./package.json'); // 包信息
 
 const less = require('gulp-less');
-const process = require('child_process');
+const child_process = require('child_process');
 
 const getFile = require('getfiles');
 
 const cf = new cfiles();
-const fileType =['js', 'jsx','vue']
+const fileType = ['js', 'jsx', 'vue']
 let timer = null;
 const date = common.tools.formatDate(new Date());
 const time = date.split(' ')[0];
 const logurl = `log/${time}-log.text`;
 
-const period = 1000*60*60;
+const period = 1000 * 60 * 60;
 
 
-const store={
-  report:[],
-  set:function(json){
-    let _this = this ;
+const store = {
+  report: [],
+  set: function (json) {
+    let _this = this;
     for (var i = 0; i < this.report.length; i++) {
-      (function (index,report) {
-        if (report[index].name === json.pname && json.errorCount >0) {
+      (function (index, report) {
+        if (report[index].name === json.pname && json.errorCount > 0) {
           let codeLine = json.source.split('\n').length;
           report[index].errorCount += json.errorCount;
-          report[index].codeLine  += codeLine;
+          report[index].codeLine += codeLine;
           // console.log(`${json.pname}===${report[index].errorCount}`);
 
           report[index]['child'].push({
             name: json.pname,
             date: time,
-            codeLine:codeLine,
+            codeLine: codeLine,
             // name2:report[index].name,
             filePath: json.filePath,
             errorCount: json.errorCount
           });
         }
-      })(i,_this.report);
+      })(i, _this.report);
     }
   },
-  clear:function(){
-    this.report=[];
+  clear: function () {
+    this.report = [];
   },
-  add:function(item,time){
+  add: function (item, time) {
     this.report.push({
       name: item.name,
       errorCount: 0,
-      codeLine:0,
+      codeLine: 0,
       date: time,
       child: []
     });
@@ -65,58 +65,69 @@ const store={
 
 
 class action {
-  constructor(){
-    this.reg =  new RegExp(white.join('|'));
+  constructor() {
+    this.reg = new RegExp(white.join('|'));
   }
-  init(){
+  init() {
     var _this = this;
     gulp.task('s', (req, res) => {
       console.log(`welcome to use ${app.name}${app.version}`);
       connect.server();
     });
 
-    gulp.task('pull',function(){
+    gulp.task('pull', function () {
       _this.doPull();
     });
 
-    gulp.task('default',function(){
+    gulp.task('default', function () {
       _this.doPull();
-      var timer = setInterval(function(){
+      var timer = setInterval(function () {
         _this.doPull();
-      },period);
+      }, period);
+    });
+    gulp.task('delete', function () {
+      fs.unlink('store', err => {
+        if (err) {
+          console.log(err);
+        }
+        console.log('del succedd');
+      })
     });
   }
-  doPull(){
+  doPull() {
     const _this = this;
-    store.clear();// 清空report
+    store.clear(); // 清空report
     // const getfile = new getFile();// 重置getfile
 
-    Promise.all(project.map(function(item,index){
-      process.exec(`git clone ${item.store} "store/${item.name}"`, (error, stdout, stderr)=>{
+    Promise.all(project.map(function (item, index) {
+      child_process.exec('rm -rf store', (error, stdout, stderr) => {
+        child_process.exec(`git clone ${item.store} "store/${item.name}"`, (error, stdout, stderr) => {
 
-        common.file.set(logurl,`${date}:${item.name}结束clone\n`);
-        item.getfile =  new getFile();
+          common.file.set(logurl, `${date}:${item.name}结束clone\n`);
+          item.getfile = new getFile();
 
-        store.add(item,time);
+          store.add(item, time);
 
-        item.getfile.getResult({
-          root: item.url,
-          suffix: fileType,
-          callback: function (list) {
-            _this.eslintList(list);
-          }
+          item.getfile.getResult({
+            root: item.url,
+            suffix: fileType,
+            callback: function (list) {
+              _this.eslintList(list);
+            }
+          });
+
+
         });
-
-
       });
+
     })).then();
   }
   eslintList(list) {
     const _this = this;
     Promise.all(list.map(file => {
-      if(_this.isWhite(file)){
+      if (_this.isWhite(file)) {
         return false;
-      }else{
+      } else {
         let filePromise = new Promise((resolve, reject) => {
           gulp.src(file).pipe(eslint()).pipe(eslint.format('json', result => {
             const pname = file.substring(file.indexOf('store')).split('/')[1]; // 当前eslint的项目名称
@@ -139,46 +150,46 @@ class action {
 
     }));
   }
-  createResult(json){
+  createResult(json) {
     store.set(json);
-    const allJSON=`result/all.json`;
+    const allJSON = `result/all.json`;
     timer = setTimeout(function () {
       clearTimeout(timer);
 
-      common.file.set(logurl,`${date}:eslint 结束\n`);
-      common.file.set(logurl,`============================\n`);
+      common.file.set(logurl, `${date}:eslint 结束\n`);
+      common.file.set(logurl, `============================\n`);
 
 
       // console.log('=========eslint end==============');
 
       cf.create(`result/${time}-all.json`, JSON.stringify(store.report));
-      let pArr=[];
-      for(let i =0;i<store.report.length;i++){
+      let pArr = [];
+      for (let i = 0; i < store.report.length; i++) {
         pArr.push({
-          date:time,
-          name:store.report[i].name,
-          errorCount:store.report[i].errorCount
+          date: time,
+          name: store.report[i].name,
+          errorCount: store.report[i].errorCount
         });
       }
 
-       common.file.read(allJSON).then(data=>{
-         let list = JSON.parse(data);
-         // 当前时间和最后一个匹配，则覆盖写入
-         if(list[list.length-1][0].date === pArr[0].date){
-           list.pop();
-         }
-         list.push(pArr);
-         common.file.reset(allJSON,JSON.stringify(list),function(){});
-       }).catch(err=>{
-         let list =[];
-         list.push(pArr);
-         cf.create(allJSON,JSON.stringify(list),function(){});
-       });
+      common.file.read(allJSON).then(data => {
+        let list = JSON.parse(data);
+        // 当前时间和最后一个匹配，则覆盖写入
+        if (list[list.length - 1][0].date === pArr[0].date) {
+          list.pop();
+        }
+        list.push(pArr);
+        common.file.reset(allJSON, JSON.stringify(list), function () {});
+      }).catch(err => {
+        let list = [];
+        list.push(pArr);
+        cf.create(allJSON, JSON.stringify(list), function () {});
+      });
 
     }, 1000);
   }
 
-  isWhite(file){
+  isWhite(file) {
     let name = file.toLowerCase();
     return this.reg.test(name);
   }
